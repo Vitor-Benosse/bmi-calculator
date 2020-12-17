ed4d88c3c699a3e3174488a8cbbaeb685d6c8726
Vitor Benosse <vitorbenosse@gmail.com>
2020-12-17 15:47:41 -0300

    Refactoring some code

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -28,18 +28,17 @@ class MainActivity : AppCompatActivity() {
     private fun calculateBmi() {
         val weightInput = binding.weightInput.text.toString()
         val heightInput = binding.heightInput.text.toString()
-        val peopleIcon = binding.peopleIcon
 
         if(weightInput.isNotEmpty() && heightInput.isNotEmpty()) {
             val calculateBmi = weightInput.toFloat() / (heightInput.toFloat() * heightInput.toFloat())
             val bmi = calculateBmi * 10000
-            binding.apply {
+            binding.peopleIcon.apply {
                 when {
-                    bmi <= 18 -> peopleIcon.setColorFilter(getColor(R.color.orange))
+                    bmi <= 18.9 -> setColorFilter(getColor(R.color.orange))
 
-                    bmi.toInt() in 19..24 -> peopleIcon.setColorFilter(getColor(R.color.green))
+                    bmi.toInt() in 19..24 -> setColorFilter(getColor(R.color.green))
 
-                    else -> peopleIcon.setColorFilter(getColor(R.color.red))
+                    else -> setColorFilter(getColor(R.color.red))
                 }
             }
         } else {
@@ -49,18 +48,18 @@ class MainActivity : AppCompatActivity() {
 
     @RequiresApi(Build.VERSION_CODES.M)
     private fun changeColorTimestamp() {
-        binding.apply {
+        binding.peopleIcon.apply {
             when (getTimestamp()) {
-                0 -> peopleIcon.setColorFilter(getColor(R.color.white))
-                1 -> peopleIcon.setColorFilter(getColor(R.color.black))
-                2 -> peopleIcon.setColorFilter(getColor(R.color.blue))
-                3 -> peopleIcon.setColorFilter(getColor(R.color.green))
-                4 -> peopleIcon.setColorFilter(getColor(R.color.pink))
-                5 -> peopleIcon.setColorFilter(getColor(R.color.red))
-                6 -> peopleIcon.setColorFilter(getColor(R.color.purple_200))
-                7 -> peopleIcon.setColorFilter(getColor(R.color.yellow))
-                8 -> peopleIcon.setColorFilter(getColor(R.color.gray))
-                9 -> peopleIcon.setColorFilter(getColor(R.color.lilac))
+                0 -> setColorFilter(getColor(R.color.white))
+                1 -> setColorFilter(getColor(R.color.black))
+                2 -> setColorFilter(getColor(R.color.blue))
+                3 -> setColorFilter(getColor(R.color.green))
+                4 -> setColorFilter(getColor(R.color.pink))
+                5 -> setColorFilter(getColor(R.color.red))
+                6 -> setColorFilter(getColor(R.color.purple_200))
+                7 -> setColorFilter(getColor(R.color.yellow))
+                8 -> setColorFilter(getColor(R.color.gray))
+                9 -> setColorFilter(getColor(R.color.lilac))
             }
         }
     }
